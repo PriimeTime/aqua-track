7d3cc7442cd0df95da2158d1f88e27385a134ddb
PriimeTime <andor98@gmail.com>
2024-03-18 19:25:30 +0100

    ADDED custom text field component

---
+++ src/components/input/CustomTextField.jsx
@@ -0,0 +1,114 @@
+import { StyleSheet, View, TextInput, Text } from "react-native";
+import { SecondaryText } from "../texts/SecondaryText";
+import SCREEN_SIZE from "../../utils/screenSize";
+import { color } from "../../utils/themes";
+
+const textFontValues = {
+  1: { fontSize: 12 },
+  2: { fontSize: 14 },
+  3: { fontSize: 16 },
+  4: { fontSize: 18 },
+  5: { fontSize: 20 },
+  6: { fontSize: 24 },
+  7: { fontSize: 36 },
+  8: { fontSize: 48 },
+};
+
+const appendFontValues = {
+  1: { fontSize: 9 },
+  2: { fontSize: 11 },
+  3: { fontSize: 13 },
+  4: { fontSize: 15 },
+  5: { fontSize: 17 },
+  6: { fontSize: 20 },
+  7: { fontSize: 26 },
+  8: { fontSize: 30 },
+};
+
+const titleSize = {
+  SMALL: 4,
+  MEDIUM: 6,
+  LARGE: 8,
+};
+
+textFieldHeight = {
+  SMALL: 40,
+  MEDIUM: 50,
+  LARGE: 100,
+};
+
+function CustomTextField({ label, keyboardType, maxLength, append }) {
+  let textAlign = "auto";
+
+  if (keyboardType === "numeric") {
+    textAlign = "center";
+  }
+
+  return (
+    <>
+      {label && (
+        <View style={{ width: "25%" }}>
+          <SecondaryText size={titleSize[SCREEN_SIZE]} color={color.DARK_BLUE}>
+            {label}
+          </SecondaryText>
+        </View>
+      )}
+      <View
+        style={{
+          paddingRight: "2.5%",
+          paddingLeft: "2.5%",
+          width: "25%",
+          height: textFieldHeight[SCREEN_SIZE],
+          flexDirection: "row",
+          borderRadius: textFieldHeight[SCREEN_SIZE] / 2,
+          backgroundColor: color.WHITE,
+        }}
+      >
+        <TextInput
+          style={[
+            styles.textInput,
+            {
+              textAlign,
+              width: append ? "75%" : "100%",
+            },
+          ]}
+          keyboardType={keyboardType}
+          maxLength={maxLength}
+        ></TextInput>
+        {append && (
+          <View
+            style={{
+              height: "100%",
+              width: "25%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                fontFamily: "Chewy-Regular",
+                fontSize: appendFontValues[titleSize[SCREEN_SIZE]].fontSize,
+                color: color.DARK_BLUE,
+              }}
+            >
+              {append}
+            </Text>
+          </View>
+        )}
+      </View>
+    </>
+  );
+}
+
+export { CustomTextField };
+
+const styles = StyleSheet.create({
+  textInput: {
+    top: "5%",
+    height: "90%",
+    fontFamily: "Chewy-Regular",
+    fontSize: textFontValues[titleSize[SCREEN_SIZE]].fontSize,
+    color: color.DARK_BLUE,
+  },
+});
--- src/screens/settings/ProfileSettings.jsx
+++ src/screens/settings/ProfileSettings.jsx
@@ -1,7 +1,22 @@
 import { ContentPage } from "../ContentPage";
+import { CustomTextField } from "../../components/input/CustomTextField";
 
 function ProfileSettings() {
-  return <ContentPage title="Metrics & Body Measurements"></ContentPage>;
+  return (
+    <ContentPage title="Metrics & Body Measurements">
+      {/* <CustomTextField
+        keyboardType="numeric"
+        maxLength={2}
+        label="Age"
+      ></CustomTextField>
+      <CustomTextField
+        keyboardType="numeric"
+        maxLength={3}
+        label="Weight"
+        append="kg"
+      ></CustomTextField> */}
+    </ContentPage>
+  );
 }
 
 export { ProfileSettings };
