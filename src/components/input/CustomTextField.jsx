98e0e85586395a8b599291b0029edf774474c52d
PriimeTime <andor98@gmail.com>
2024-03-21 20:43:10 +0100

    REFACTORED custom text field styles

--- src/components/input/CustomTextField.jsx
+++ src/components/input/CustomTextField.jsx
@@ -53,17 +53,7 @@ function CustomTextField({ label, keyboardType, maxLength, append }) {
           </SecondaryText>
         </View>
       )}
-      <View
-        style={{
-          paddingRight: "2.5%",
-          paddingLeft: "2.5%",
-          width: "25%",
-          height: textFieldHeight[SCREEN_SIZE],
-          flexDirection: "row",
-          borderRadius: textFieldHeight[SCREEN_SIZE] / 2,
-          backgroundColor: color.WHITE,
-        }}
-      >
+      <View style={styles.textInputWrapper}>
         <TextInput
           style={[
             styles.textInput,
@@ -76,24 +66,8 @@ function CustomTextField({ label, keyboardType, maxLength, append }) {
           maxLength={maxLength}
         ></TextInput>
         {append && (
-          <View
-            style={{
-              height: "100%",
-              width: "25%",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Text
-              style={{
-                textAlign: "center",
-                fontFamily: "Chewy-Regular",
-                fontSize: appendFontValues[titleSize[SCREEN_SIZE]].fontSize,
-                color: color.DARK_BLUE,
-              }}
-            >
-              {append}
-            </Text>
+          <View style={styles.appendWrapper}>
+            <Text style={styles.append}>{append}</Text>
           </View>
         )}
       </View>
@@ -104,6 +78,27 @@ function CustomTextField({ label, keyboardType, maxLength, append }) {
 export { CustomTextField };
 
 const styles = StyleSheet.create({
+  appendWrapper: {
+    height: "100%",
+    width: "25%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  append: {
+    textAlign: "center",
+    fontFamily: "Chewy-Regular",
+    fontSize: appendFontValues[titleSize[SCREEN_SIZE]].fontSize,
+    color: color.DARK_BLUE,
+  },
+  textInputWrapper: {
+    paddingRight: "2.5%",
+    paddingLeft: "2.5%",
+    width: "25%",
+    height: textFieldHeight[SCREEN_SIZE],
+    flexDirection: "row",
+    borderRadius: textFieldHeight[SCREEN_SIZE] / 2,
+    backgroundColor: color.WHITE,
+  },
   textInput: {
     top: "5%",
     height: "90%",
