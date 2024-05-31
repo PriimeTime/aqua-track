07418cdce781882a54373091ff697b374c508efa
PriimeTime <andor98@gmail.com>
2024-05-31 21:39:09 +0200

    REFACTORED firebase.js to firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -8,13 +8,13 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "",
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || "",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "",
+  apiKey: process.env.EXPO_PUBLIC_API_KEY || "",
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN || "",
+  databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL || "",
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID || "",
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID || "",
+  appId: process.env.EXPO_PUBLIC_APP_ID || "",
 };
 
 const app = initializeApp(firebaseConfig);
